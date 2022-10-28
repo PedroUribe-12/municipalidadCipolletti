d979ace6f792f93c525fbcf16ba33d1120bbb770
PedroUribe-12 <orfeouribe@gmail.com>
2022-10-28 13:16:17 -0300

    Subimos el proyecto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Sede} from '../modelo/sede';
+import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +11,82 @@ export class HomePage {
 
   constructor() {}
 
+  slideOpts = {
+    on: {
+      beforeInit() {
+        const swiper = this;
+        swiper.classNames.push(`${swiper.params.containerModifierClass}fade`);
+        const overwriteParams = {
+          slidesPerView: 1,
+          slidesPerColumn: 1,
+          slidesPerGroup: 1,
+          watchSlidesProgress: true,
+          spaceBetween: 0,
+          virtualTranslate: true,
+        };
+        swiper.params = Object.assign(swiper.params, overwriteParams);
+        swiper.params = Object.assign(swiper.originalParams, overwriteParams);
+      },
+      setTranslate() {
+        const swiper = this;
+        const { slides } = swiper;
+        for (let i = 0; i < slides.length; i += 1) {
+          const $slideEl = swiper.slides.eq(i);
+          const offset$$1 = $slideEl[0].swiperSlideOffset;
+          let tx = -offset$$1;
+          if (!swiper.params.virtualTranslate) tx -= swiper.translate;
+          let ty = 0;
+          if (!swiper.isHorizontal()) {
+            ty = tx;
+            tx = 0;
+          }
+          const slideOpacity = swiper.params.fadeEffect.crossFade
+            ? Math.max(1 - Math.abs($slideEl[0].progress), 0)
+            : 1 + Math.min(Math.max($slideEl[0].progress, -1), 0);
+          $slideEl
+            .css({
+              opacity: slideOpacity,
+            })
+            .transform(`translate3d(${tx}px, ${ty}px, 0px)`);
+        }
+      },
+      setTransition(duration) {
+        const swiper = this;
+        const { slides, $wrapperEl } = swiper;
+        slides.transition(duration);
+        if (swiper.params.virtualTranslate && duration !== 0) {
+          let eventTriggered = false;
+          slides.transitionEnd(() => {
+            if (eventTriggered) return;
+            if (!swiper || swiper.destroyed) return;
+            eventTriggered = true;
+            swiper.animating = false;
+            const triggerEvents = ['webkitTransitionEnd', 'transitionend'];
+            for (let i = 0; i < triggerEvents.length; i += 1) {
+              $wrapperEl.trigger(triggerEvents[i]);
+            }
+          });
+        }
+      },
+    }
+  }
+
+  sedes: Sede[] = [
+  {
+    idSede:'Sede1',
+    nombre:'Municipalidad..',
+    descripcion: 'Es una sede muy bonita',
+    telefono:'299-874-3923',
+    direccion:'Primeros Pobladores 4932',
+    imagen:'../../assets/sedes/Edificio-Municipal-1024x683.jpg'
+  },
+  {
+    idSede:'Sede1',
+    nombre:'Municipalidad..',
+    descripcion: 'Es una sede muy hermosa',
+    telefono:'299-945-8323',
+    direccion:'Manuel Estrada 4932',
+    imagen:'../../assets/sedes/muni.jpg'
+  },
+]
 }
---
+++ src/app/modelo/sede.ts
@@ -0,0 +1,8 @@
+export interface Sede {
+    idSede:string;
+    nombre:string;
+    imagen:string;
+    descripcion:string;
+    telefono:string;
+    direccion:string;
+}
